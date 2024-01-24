d50b4cf8fb7ab32fd1fb50de9749c936fc96b344
Scorpio0712 <scorpiotha.0712@gmail.com>
2024-01-25 03:25:34 +0700

    Create nav and carousel

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      
       home: HomePage(),
     );
   }
 }
 
-
+class AppColor {
+  static const Color luxuryYellow = Color(0xffffe5a9);
+  static const Color luxuryBlack = Color(0xff423f3b);
+  static const Color luxuryBlue = Color(0xff525266);
+  static const Color luxuryRed = Color(0xffff6666);
+  static const Color luxuryGrey = Color(0xffcbbeb5);
+}
