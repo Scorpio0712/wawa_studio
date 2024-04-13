02b07a16ae8e318b9edda6599297214ea1d709fd
NILE <kritsaran9@gmail.com>
2024-04-13 17:26:33 +0700

    responsive

---
+++ lib/responsive_layout/mobile_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MymobileBody extends StatelessWidget {
+  const MymobileBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(backgroundColor: Colors.green[300],);
+  }
+}
---
+++ lib/responsive_layout/responsive_layout.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+import 'package:wawastudio/responsive_layout/dimension.dart';
+
+class ResponsiveLaout extends StatelessWidget {
+  final Widget mobileBody;
+  final Widget destopBody;
+
+  ResponsiveLaout({required this.mobileBody, required this.destopBody})
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: ((context, constraints) {
+        if(constraints.maxWidth < mobileWidth) {
+          return mobileBody; 
+        } else {
+          return destopBody;
+        }
+      }),
+    );
+  }
+}
